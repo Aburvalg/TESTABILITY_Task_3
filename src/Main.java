51c2c55f1b4276c2af8b6089362a127c2f869595
Andrey <3451z@mail.ru>
2022-01-10 08:57:44 +0300

    исправление ошибок

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double pay = 0;
-        double payment = service.calculate(pay);
-        System.out.println("Ваш ежемесячный платеж: " + payment + "рублей");
+        double payment1 = service.calculate(1000000, 9.99, 12);
+        double payment2 = service.calculate(1000000, 9.99, 24);
+        double payment3 = service.calculate(1000000, 9.99, 36);
+        System.out.println("Ваш ежемесячный платеж: " + payment1 + "рублей");
+        System.out.println("Ваш ежемесячный платеж: " + payment2 + "рублей");
+        System.out.println("Ваш ежемесячный платеж: " + payment3 + "рублей");
     }
 }
